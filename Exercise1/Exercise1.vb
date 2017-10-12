b0ea90e5e7fe62bdfdc286a4694dfe9a72244c79
Dennisak12 <Dennisak2@gmail.com>
2017-10-11 20:09:18 -0400

    Add files via upload

---
+++ Exercise1/Exercise1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Exercise1
+    Private Sub btnHello_Click(sender As Object, e As EventArgs) Handles btnHello.Click
+
+        'Check to see if label is visible
+        If (lblHello.Visible = False) Then
+            lblHello.Visible = True
+        Else
+            lblHello.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub lblHello_Click(sender As Object, e As EventArgs) Handles lblHello.Click
+
+    End Sub
+End Class
