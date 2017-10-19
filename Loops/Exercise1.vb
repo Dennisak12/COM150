d3220daf3bb3ca1aeded4adb99eded95ed2ec018
Dennisak12 <Dennisak2@gmail.com>
2017-10-18 20:12:06 -0400

    Add files via upload

---
+++ Loops/Exercise1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Exercise1
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+
+
+
+        'declare variable
+        Dim n As Integer
+
+        'start for loop
+        For n = 1 To 10
+            lstOut.Items.Add(n)
+        Next
+
+
+    End Sub
+End Class
